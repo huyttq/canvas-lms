2c9d5b3185c307a80954c70bedac0a2bc93512f7
Huy Thai <huyttq@gmail.com>
2020-07-19 22:29:25 +0800

    Use non-cluster mode

--- config/puma.rb
+++ config/puma.rb
@@ -22,19 +22,18 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads 0, max_threads_count
 
-bind "unix:///var/run/puma/my_app.sock"
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+# Specifies the `environment` that Puma will run in.
+environment rails_env
+
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads 0, 4
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
@@ -44,14 +43,21 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 0 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+#preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if rails_env = 'production'
+  bind "unix:///var/run/puma/my_app.sock"
+
+  # Log puma output to files
+  stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
+end
