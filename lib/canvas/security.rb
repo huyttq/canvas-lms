e5da938fb9277a69ee78e4715e42ee13b68f6e74
Huy Thai <huyttq@gmail.com>
2021-03-08 11:46:05 +0800

    Submission exemption for Trainee role

--- lib/canvas/security.rb
+++ lib/canvas/security.rb
@@ -29,6 +29,14 @@ module Canvas
     # as the shim, but we have several other classes actually inside
     # lib/canvas/security/*.rb that need the existing module structure,
     # so method_missing works better at the moment.
+  def self.whitelist_roles
+    @whitelist_roles ||= begin
+        res = config && config['whitelist_roles']
+        raise('whitelist_roles required, see config/security.yml') unless res
+        res
+      end
+  end
+
     class << self
       def method_missing(m, *args, &block)
         CanvasSecurity.send(m, *args, &block)
