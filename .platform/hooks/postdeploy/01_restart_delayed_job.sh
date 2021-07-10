cba754f46bffc869036065c62f88e122fca0872c
Huy Thai <huyttq@gmail.com>
2021-07-10 17:01:20 +0800

    Migrate to Amazon Linux 2

--- .platform/hooks/postdeploy/01_restart_delayed_job.sh
+++ .platform/hooks/postdeploy/01_restart_delayed_job.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env bash
+
 # Adds a post-deploy hook such that after a new version is deployed
 # successfully, restarts the delayed_job workers.
 #
 # http://stackoverflow.com/questions/14401204/how-to-automatically-restart-delayed-job-when-deploying-a-rails-project-on-amazo
 # http://www.dannemanne.com/posts/post-deployment_script_on_elastic_beanstalk_restart_delayed_job
-
-#!/usr/bin/env bash
 
 # Start delayed_job
 /bin/bash -c "/var/app/current/script/canvas_init restart"
---
+++ .platform/hooks/prebuild/01_update_permissions.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+chmod +x /var/app/staging/vendor/QTIMigrationTool/migrate.py
