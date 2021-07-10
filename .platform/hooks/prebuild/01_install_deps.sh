44c4074220f1c0b0c488be12c78540ec0b1831c7
Huy Thai <huyttq@gmail.com>
2021-07-10 16:35:57 +0800

    permission fix

--- .platform/hooks/prebuild/01_install_deps.sh
+++ .platform/hooks/prebuild/01_install_deps.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-yum install git -y
-yum install xmlsec1-devel -y
-yum install libtool-ltdl-devel -y
-yum install xmlsec1-openssl -y
+sudo yum install git -y
+sudo yum install xmlsec1-devel -y
+sudo yum install libtool-ltdl-devel -y
+sudo yum install xmlsec1-openssl -y
 # gem install nokogiri-xmlsec-instructure -v '0.9.6' --source "https://rubygems.org/"
 # gem install bundler -v 2.2.17
-yum install python-pip -y
-pip install lxml
-chmod +x /var/app/staging/vendor/QTIMigrationTool/migrate.py
+sudo yum install python-pip -y
+sudo pip install lxml
+sudo chmod +x /var/app/staging/vendor/QTIMigrationTool/migrate.py
